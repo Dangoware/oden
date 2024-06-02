b19f6f108cf5759ff128ee606e3f69d1eb66294c
G2-Games <ke0bhogsg@gmail.com>
2024-06-02 06:17:45 -0500

    Initial commit

---
+++ src/commands.rs
@@ -0,0 +1,162 @@
+use std::process::{Child, Command, Stdio};
+
+use serenity::async_trait;
+use songbird::{input::ChildContainer, Event, EventContext, EventHandler as VoiceEventHandler, TrackEvent};
+use url::Url;
+
+use crate::{Context, Error};
+
+struct TrackErrorNotifier;
+
+#[async_trait]
+impl VoiceEventHandler for TrackErrorNotifier {
+    async fn act(&self, ctx: &EventContext<'_>) -> Option<Event> {
+        if let EventContext::Track(track_list) = ctx {
+            for (state, handle) in *track_list {
+                println!(
+                    "Track {:?} encountered an error: {:?}",
+                    handle.uuid(),
+                    state.playing
+                );
+            }
+        }
+
+        None
+    }
+}
+
+/// Show this help menu
+#[poise::command(prefix_command, track_edits)]
+pub async fn help(
+    ctx: Context<'_>,
+    #[description = "Specific command to show help about"]
+    #[autocomplete = "poise::builtins::autocomplete_command"]
+    command: Option<String>,
+) -> Result<(), Error> {
+    poise::builtins::help(
+        ctx,
+        command.as_deref(),
+        poise::builtins::HelpConfiguration {
+            extra_text_at_bottom: "This is not a bot",
+            ..Default::default()
+        },
+    )
+    .await?;
+    Ok(())
+}
+
+#[poise::command(prefix_command, track_edits)]
+pub async fn join(
+    ctx: Context<'_>,
+) -> Result<(), Error> {
+    let (guild_id, channel_id) = {
+        let guild = ctx.guild().unwrap();
+        let channel_id = guild
+            .voice_states
+            .get(&ctx.author().id)
+            .and_then(|voice_state| voice_state.channel_id);
+
+        (guild.id, channel_id)
+    };
+
+    let connect_to = match channel_id {
+        Some(channel) => channel,
+        None => {
+            //check_msg(msg.reply(ctx, "Not in a voice channel").await);
+
+            return Ok(());
+        },
+    };
+
+    let manager = songbird::get(ctx.serenity_context())
+        .await
+        .expect("Songbird Voice client placed in at initialisation.")
+        .clone();
+
+    if let Ok(handler_lock) = manager.join(guild_id, connect_to).await {
+        // Attach an event handler to see notifications of all track errors.
+        let mut handler = handler_lock.lock().await;
+        handler.add_global_event(TrackEvent::Error.into(), TrackErrorNotifier);
+    }
+
+    Ok(())
+}
+
+#[poise::command(prefix_command, track_edits)]
+pub async fn leave(
+    ctx: Context<'_>,
+) -> Result<(), Error> {
+    let guild_id = ctx.guild_id().unwrap();
+
+    let manager = songbird::get(ctx.serenity_context())
+        .await
+        .expect("Songbird Voice client placed in at initialisation.")
+        .clone();
+    let has_handler = manager.get(guild_id).is_some();
+
+    if has_handler {
+        manager.remove(guild_id).await?;
+
+        ctx.channel_id().say(&ctx.http(), "Left voice channel").await?;
+    } else {
+        ctx.reply("Not in a voice channel").await?;
+    }
+
+    Ok(())
+}
+
+#[poise::command(prefix_command, track_edits)]
+pub async fn play(
+    ctx: Context<'_>,
+    url: Option<Url>
+) -> Result<(), Error> {
+    let url = match url {
+        Some(url) => url,
+        None => {
+            ctx.channel_id()
+                .say(&ctx.http(), "Must provide a URL to a video or audio")
+                .await?;
+
+            return Ok(());
+        },
+    };
+
+    let do_search = !url.as_str().starts_with("http");
+
+    let guild_id = ctx.guild_id().unwrap();
+
+    let manager = songbird::get(ctx.serenity_context())
+        .await
+        .expect("Songbird Voice client placed in at initialisation.")
+        .clone();
+
+    if let Some(handler_lock) = manager.get(guild_id) {
+        let mut handler = handler_lock.lock().await;
+
+        let yt_dlp = Command::new("yt-dlp")
+            .args(vec![
+                "-f", "bestaudio",
+                "-o", "-",
+                url.as_str()
+            ])
+            .stdout(Stdio::piped())
+            .spawn()
+            .unwrap();
+
+        let src = ChildContainer::new(
+            vec![
+                yt_dlp,
+
+            ]
+        );
+        let _ = handler.play_input(src.into());
+
+        ctx.channel_id().say(&ctx.http(), "Playing song").await?;
+    } else {
+        ctx.channel_id()
+            .say(&ctx.http(), "Not in a voice channel to play in")
+            .await?;
+    }
+
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,123 @@
+#![warn(clippy::str_to_string)]
+
+mod commands;
+
+use poise::serenity_prelude as serenity;
+use std::{
+    env::var,
+    sync::Arc,
+    time::Duration,
+};
+
+use songbird::SerenityInit;
+
+// Types used by all command functions
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+// Custom user data passed to all command functions
+pub struct Data {
+}
+
+async fn on_error(error: poise::FrameworkError<'_, Data, Error>) {
+    // This is our custom error handler
+    // They are many errors that can occur, so we only handle the ones we want to customize
+    // and forward the rest to the default handler
+    match error {
+        poise::FrameworkError::Setup { error, .. } => panic!("Failed to start bot: {:?}", error),
+        poise::FrameworkError::Command { error, ctx, .. } => {
+            println!("Error in command `{}`: {:?}", ctx.command().name, error,);
+        }
+        error => {
+            if let Err(e) = poise::builtins::on_error(error).await {
+                println!("Error while handling error: {}", e)
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let options = poise::FrameworkOptions {
+        // Set up commands
+        commands: vec![
+            commands::help(),
+            commands::join(),
+            commands::leave(),
+            commands::play(),
+        ],
+
+        prefix_options: poise::PrefixFrameworkOptions {
+            prefix: Some("!".into()),
+            edit_tracker: Some(Arc::new(poise::EditTracker::for_timespan(
+                Duration::from_secs(3600),
+            ))),
+            ..Default::default()
+        },
+
+        // The global error handler for all error cases that may occur
+        on_error: |error| Box::pin(on_error(error)),
+
+        // This code is run before every command
+        pre_command: |ctx| {
+            Box::pin(async move {
+                println!("Executing command {}...", ctx.command().qualified_name);
+            })
+        },
+
+        // This code is run after a command if it was successful (returned Ok)
+        post_command: |ctx| {
+            Box::pin(async move {
+                println!("Executed command {}!", ctx.command().qualified_name);
+            })
+        },
+
+        event_handler: |_ctx, event, _framework, _data| {
+            Box::pin(async move {
+                println!(
+                    "Got an event in event handler: {:?}",
+                    event.snake_case_name()
+                );
+                Ok(())
+            })
+        },
+        ..Default::default()
+    };
+
+    let framework = poise::Framework::builder()
+        .setup(move |ctx, _ready, framework| {
+            Box::pin(async move {
+                println!("Logged in as \"{}\"", _ready.user.name);
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {
+                    // data goes here
+                })
+            })
+        })
+        .options(options)
+        .build();
+
+    let token = var("DISCORD_TOKEN")
+        .expect("Missing `DISCORD_TOKEN` env var");
+    let intents =
+        serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = serenity::ClientBuilder::new(token, intents)
+        .framework(framework)
+        .register_songbird()
+        .await
+        .unwrap();
+
+    tokio::spawn(async move {
+        let _ = client
+            .start()
+            .await
+            .map_err(|why| println!("Client ended: {:?}", why));
+    });
+
+    let _signal_err = tokio::signal::ctrl_c().await;
+
+    println!("\rReceived Ctrl-C, shutting down.");
+}
